4fc0e76fed6c530bf72a46211032ed4f7b072a1b
Zack Liscio <zackliscio@gmail.com>
2012-12-30 11:50:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprezzyrank::Application.config.secret_token = '3390789d295f7e8497f7f2e0ad8eeb8275d211278b4ad59301024077f2c1a2bb1cbc569d34431350035057b0d8c647990ca257d4d13911a906f1e0f21551efed'
